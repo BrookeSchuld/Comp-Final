9294350ee3a1bef628f2bfa1affcf217b3bc3556
BrookeSchuld <109992605+BrookeSchuld@users.noreply.github.com>
2023-04-10 21:26:35 -0600

    Add files via upload

---
+++ ncc_dedx.py
@@ -0,0 +1,247 @@
+import ROOT as RT
+from ROOT import TLorentzVector
+import numpy as np
+import sys
+import lar_functions as lar
+import math
+
+kkBlue=RT.TColor(9000,   0/255., 119/255., 187/255.)
+kkOrange  = RT.TColor(9003, 238/255., 119/255.,  51/255.)
+
+edep_tree=RT.TChain("EDepSimEvents")
+grtk_tree=RT.TChain("DetSimPassThru/gRooTracker")
+filelist=[sys.argv[x] for x in range(1,len(sys.argv))]
+for file in filelist:
+    edep_tree.Add(file)
+    grtk_tree.Add(file)
+
+def is_point_contained(pos):                 ##Check to see if the position is within the 2x2 
+    if abs(pos[0])>670: return False
+    if abs(pos[1]-430)>670:return False
+    if abs(pos[2])>670:return False
+    return True
+    
+def statistics(evt,vtx,part_id,trk,mass):
+    energy=[]
+    vtx_pos=vtx.GetPosition().Vect()
+    position=traj[part_id].Points[-1].GetPosition().Vect()
+    distance=(vtx_pos-position).Mag()/10
+    for j in traj[part_id].Points:
+        energy.append(np.sqrt(j.GetMomentum().Mag()**2+mass**2))
+    DE=np.sum(-np.diff(energy))
+    KE=(trk.GetInitialMomentum().E()-trk.GetInitialMomentum().M())
+    KE_final=(energy[-2]/mass-1)*mass
+    return distance,DE,KE_final
+def reco(part_id):
+    reco_energy=0
+    de_dx=[]
+    for seg in edep_tree.Event.SegmentDetectors:
+                nChunks=len(seg[1])
+                for n in range(nChunks):
+                    key_contrib=seg[1][n].GetContributors()[0]
+                    #print(is_2x2_contained(seg[1][n].GetStop()))
+                    if part_id==key_contrib:
+                        reco_energy+=seg[1][n].GetEnergyDeposit()
+                        seg_length=(seg[1][n].GetStart()-seg[1][n].GetStop()).Mag()/10
+                        de_dx.append(-seg[1][n].GetEnergyDeposit()/seg_length)
+                    #for i in proton_children:
+                    #    if i==key_contrib:
+                    #if proton_id==key_contrib:
+                     #       reco_energy+=seg[1][n].GetEnergyDeposit()
+    return reco_energy,de_dx
+def residual(part_id):
+    for seg in edep_tree.Event.SegmentDetectors:
+        nChunks=len(seg[1])
+        for n in range(nChunks):
+            key_contrib=seg[1][n].GetContributors()[0]
+            if part_id==key_contrib:
+                seg_length=(seg[1][n].GetStart()-seg[1][n].GetStop()).Mag()/10
+                de_dx=-seg[1][n].GetEnergyDeposit()/seg_length
+                residual=(seg[1][n].GetStart()-traj[part_id].Points[-1].GetPosition()).Mag()/10
+                #if residual>seg_length: res_range.Fill(residual,de_dx)
+                res_range.Fill(residual,de_dx)
+    return   
+############################################################################## Main Code #################################################
+proton_mass=938.27 ##MeV/c^2
+pion_mass=135.0
+nevt=edep_tree.GetEntries()
+################ Initialized Variables #################
+Proton_Energy_Dep=[]
+Pion_Energy_Dep=[]
+Proton_KE=[]
+Pion_KE=[]
+Proton_Reco=[]
+Pion_Reco=[]
+Proton_Dedx=[]
+Pion_Dedx=[]
+
+res_range=RT.TH2D("h","residual range",100,0,30,50,0,80)
+
+for evt in range(nevt):
+    edep_tree.GetEntry(evt)
+    grtk_tree.GetEntry(evt)
+    vtx=edep_tree.Event.Primaries[0]
+    primary_pdg=[x.GetPDGCode() for x in vtx.Particles]
+    traj=edep_tree.Event.Trajectories
+######################## PROTONS ############################
+    if primary_pdg==[14,2212]:                            #Checking for ncc events
+        print("proton at event",evt)
+        for trk in traj: 
+            if trk.GetPDGCode()==2212:                    #Pick only the first proton
+                proton_id=trk.GetTrackId()
+                proton_trk=trk
+                break
+        #if is_point_contained(traj[proton_id].Points[-1].GetPosition().Vect())==False:
+        #        break
+        dist,DE,KE=(statistics(evt,vtx,proton_id,trk,proton_mass))
+        #residual(proton_id)
+        Proton_KE.append(KE)
+        Proton_Energy_Dep.append(DE)
+        reco_energy,de_dx=reco(proton_id)
+        Proton_Reco.append(reco_energy)
+        Proton_Dedx.append(de_dx)
+########################## PIONS #########################################
+    pion_traj=tuple(x for x in vtx.Particles if x.GetPDGCode() in [-211,211])
+    for i in primary_pdg: 
+        if abs(i)==211:
+            for trk in traj: 
+                if abs(trk.GetPDGCode())==211:
+                    pion_id=trk.GetTrackId()
+                    pion_trk=trk
+                    break
+            if is_point_contained(traj[pion_id].Points[-1].GetPosition().Vect())==False: 
+                break
+            dist,DE,KE=statistics(evt,vtx,pion_id,pion_trk,pion_mass)
+            Pion_Energy_Dep.append(DE)
+            residual(pion_id)
+            Pion_KE.append(KE)
+            reco_energy,de_dx=reco(pion_id)
+            Pion_Reco.append(reco_energy)
+            Pion_Dedx.append(de_dx)
+######## PROTON GRAPHS ##################################          
+Proton_Energy=RT.TGraph()
+Proton_DE=RT.TGraph()
+can1=RT.TCanvas("can","can",1000,800)
+can1.cd()
+for i in range(len(Proton_KE)):
+    Proton_Energy.SetPoint(i,i,Proton_KE[i])
+    Proton_DE.SetPoint(i,i,Proton_Energy_Dep[i])
+Proton_Energy.SetMarkerColor(9003)
+Proton_Energy.GetXaxis().SetTitle("Event Number")
+Proton_Energy.GetYaxis().SetTitle("Energy(MeV)")
+Proton_DE.Draw("apl")
+Proton_Energy.Draw("* same")
+RT.gPad.Update()
+can1.SaveAs("Proton_Energy_Dep.png")
+
+########### PION GRAPHS ################################
+can2=RT.TCanvas("can2","can2",1000,800)
+Pion_Energy=RT.TGraph()
+Pion_DE=RT.TGraph()
+can2.cd()
+for i in range(len(Pion_KE)):
+    Pion_Energy.SetPoint(i,i,Pion_KE[i])
+    Pion_DE.SetPoint(i,i,Pion_Energy_Dep[i])
+Pion_Energy.SetLineColor(9003)
+Pion_Energy.GetXaxis().SetTitle("Event Number")
+Pion_Energy.GetYaxis().SetTitle("Energy(MeV)")
+Pion_DE.Draw("apl")
+Pion_Energy.Draw("* same")
+RT.gPad.Update()
+can2.SaveAs("Pion_Energy_Dep.png")
+
+########### BETHE_BLOCH ####################################
+pion_mass=139.57 ##MeV
+proton_mass= 938.27##MeV
+electron_mass=.511 #MeV
+c=3*10**8
+LAr_density=1.38 #g/cm^3
+K=.307075 #MeE/mol cm^2
+z=1
+LAr_mass=39.948 #g/mol
+LAr_number=18
+I=188*10**(-6)
+B=2.0*np.log(10)
+X0=.201
+X1=3.00
+delta=[]
+a  = 0.196
+m  = 3.000
+C  = 5.217
+
+p=np.linspace(20,6000)
+beta=p/np.sqrt(p**2+pion_mass**2)
+gamma=1/np.sqrt(1-beta**2)
+Wmax=2*electron_mass*beta**2*gamma**2/(1+2*gamma*electron_mass/c**2/pion_mass+(electron_mass/c**2/pion_mass)**2)
+de_dx=[]
+X = np.log10(beta * gamma)
+for i in range(len(p)):
+    if X0<X[i] and X[i]<X1:
+        delta.append(B*X[i]+a*np.power(X1-X[i],m)-C)
+    if X[i]>X1:
+        delta.append(B*X[i]-C)
+    else:
+        delta.append(0)
+for i in range(len(p)):
+    de_dx.append(K*LAr_density*z**2*LAr_number/LAr_mass/beta[i]**2*(1/2*np.log(2*electron_mass*beta[i]**2*gamma[i]**2*Wmax[i]/I**2)-beta[i]**2-.5*delta[i]))
+################### DE_DX #########################################
+
+can3=RT.TCanvas("can3","can3",1000,800)
+#Pion_Dep=RT.TGraph()
+Pion_Dep=RT.TH2D("h1","Pion de/dx",100,0,6000,50,0,80)
+#Proton_Dep=RT.TGraph()
+for i in range(len(Pion_KE)):
+    Pion_Dep.Fill(Pion_KE[i],np.mean(Pion_Dedx[i][-3:-1]))
+Pion_Dep.Draw("colz")
+Pion_Curve=RT.TGraph(len(de_dx),(gamma-1)*pion_mass,np.array(de_dx))
+Pion_Curve.Draw("same")
+Pion_Dep.GetXaxis().SetTitle("Kinetic Energy(MeV)")
+Pion_Dep.GetYaxis().SetTitle("de_dx(MeV/cm)")
+RT.gPad.Update()
+can3.SaveAs("Pion_DEDX_hist.png")
+
+p=np.linspace(150,5000)
+beta=p/np.sqrt(p**2+proton_mass**2)
+gamma=1/np.sqrt(1-beta**2)
+
+X = np.log10(beta * gamma)
+for i in range(len(p)):
+    if X0<X[i] and X[i]<X1:
+        delta.append(B*X[i]+a*np.power(X1-X[i],m)-C)
+    if X[i]>X1:
+        delta.append(B*X[i]-C)
+    else:
+        delta.append(0)
+
+Wmax=2*electron_mass*beta**2*gamma**2/(1+2*gamma*electron_mass/c**2/proton_mass+(electron_mass/c**2/proton_mass)**2)
+de_dx=[]
+for i in range(len(p)): 
+    de_dx.append(K*z**2*LAr_density*LAr_number/LAr_mass/beta[i]**2*(1/2*np.log(2*electron_mass*beta[i]**2*gamma[i]**2*Wmax[i]/I**2)-beta[i]**2-.5*delta[i]))
+
+pion_range=[]
+E=np.sqrt(p**2+pion_mass**2)
+for i in range(len(p)):
+    delE=np.diff(E)
+    integrand=np.trapz(1/np.array(de_dx[0:i])*delE[0:i])
+    pion_range.append(integrand)
+can4=RT.TCanvas("can4","can4",1000,800)
+Proton_Dep=RT.TH2D("h2","Proton de/dx",100,0,6000,50,0,80)
+for i in range(len(Proton_KE)):
+    Proton_Dep.Fill(Proton_KE[i],np.mean(Proton_Dedx[i][-3:-1]))
+Proton_Dep.Draw("colz")
+Proton_Curve=RT.TGraph(len(de_dx),(gamma-1)*proton_mass,np.array(de_dx))
+Proton_Curve.Draw("same")
+Proton_Dep.GetXaxis().SetTitle("Kinetic Energy(MeV)")
+Proton_Dep.GetYaxis().SetTitle("de_dx(MeV/cm)")
+RT.gPad.Update()
+can4.SaveAs("Proton_DEDX_hist.png")
+
+############################# Residuals ############################
+can5=RT.TCanvas("can5","can5",1000,800)
+res_range.Draw("colz")
+Pion_Range_Curve=RT.TGraph(len(de_dx),np.array(pion_range),np.array(de_dx))
+Pion_Range_Curve.Draw("same")
+res_range.GetXaxis().SetTitle("range (cm)")
+res_range.GetYaxis().SetTitle("de_dx(MeV/cm")
+RT.gPad.Update()
+can5.SaveAs("res_range.png")
